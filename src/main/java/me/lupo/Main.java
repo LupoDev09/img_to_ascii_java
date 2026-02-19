9afd4558b615900d5a93bdae2a66a818390913e3
Tiberias23 <tiberias.peters@gmail.com>
2026-02-19 16:17:31 +0100

    add basic implementation for image to ascii conversion :3

--- src/main/java/me/lupo/Main.java
+++ src/main/java/me/lupo/Main.java
@@ -7,7 +7,7 @@ import java.io.File;
 import java.io.IOException;
 
 public class Main {
-    String ASCII = "";
+    static String ASCII = "@%#*+=-:. ";
 
     static void main() throws IOException {
         BufferedImage img = null;
@@ -19,15 +19,15 @@ public class Main {
         }
         IO.println("Loaded Image :3");
         IO.println("try image to ascii conversion");
-        IO.println(imageToAsciiConverter(img, img.getHeight(), img.getWidth()));
+        System.out.println(imageToAsciiConverter(img));
     }
 
-    private static String imageToAsciiConverter(BufferedImage img, int img_height, int img_width){
+    private static String imageToAsciiConverter(BufferedImage img) {
         StringBuilder frame = new StringBuilder();
 
-        for(int y = 0; y < img_height; y++){
-            for (int x = 0; x < img_width; x++){
-                frame.append(getAscii(img.getRGB()));
+        for (int y = 0; y < img.getHeight(); y++) {
+            for (int x = 0; x < img.getWidth(); x++) {
+                frame.append(getAscii(img.getRGB(x, y)));
             }
             frame.append('\n');
         }
@@ -35,7 +35,17 @@ public class Main {
         return frame.toString();
     }
 
-    private static int getAscii(int r, int g, int b){
+    private static char getAscii(int rgb) {
+        int r = (rgb >> 16) & 0xff;
+        int g = (rgb >> 8) & 0xff;
+        int b = rgb & 0xff;
 
+        // Grauwert (einfacher Durchschnitt)
+        int gray = (r + g + b) / 3;
+
+        // Map auf ASCII
+        int index = (gray * (ASCII.length() - 1)) / 255;
+
+        return ASCII.charAt(index);
     }
 }

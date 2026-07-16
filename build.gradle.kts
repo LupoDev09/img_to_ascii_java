a00ff0409beea4b18fe9b6a869938f9a8d5022bb
Tiberias23 <tiberias.peters@gmail.com>
2026-07-17 00:50:57 +0200

    perf: improved build time by using shadow jar :3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java")
     application
+    id("com.gradleup.shadow") version "9.0.0"
 }
 
 group = "me.lupo"
@@ -19,19 +20,20 @@ dependencies {
     implementation("net.sf.jopt-simple:jopt-simple:5.0.4") // For arg parsing
     implementation("org.jetbrains:annotations:24.1.0") // For annotations
 
-    implementation("org.bytedeco:javacv-platform:1.5.13") // Für ffmpeg
+    // Für ffmpeg
+    implementation("org.bytedeco:javacv:1.5.13")
+    implementation("org.bytedeco:ffmpeg:6.1.1-1.5.10:windows-x86_64")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
-tasks.jar {
+tasks.shadowJar {
+    archiveClassifier.set("") // ersetzt die normale jar
     manifest {
         attributes["Main-Class"] = "me.lupo.Main"
     }
-    from(configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) })
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 tasks.test {
